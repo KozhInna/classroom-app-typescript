e1447f857a0fdfd336b0193b095f7e50115a4ac3
Inna Kozhina <inna.kozhina22@gmail.com>
2024-05-24 15:50:11 +0300

    functions solution

--- src/classroom.ts
+++ src/classroom.ts
@@ -1,24 +1,31 @@
-export function getNumberOfGrades(grades) {
-    
+export function getNumberOfGrades(grades: number[]): number {
+  return grades.length;
 }
 
-export function getSumGrades(grades) {
-    
+export function getSumGrades(grades: number[]): number {
+  let sum = 0;
+  for (let grade of grades) {
+    sum += grade;
+  }
+  return sum;
 }
 
-export function getAverageValue(grades) {
-    
+export function getAverageValue(grades: number[]): number {
+  if (grades.length === 0) {
+    return 0;
+  }
+  const sum = getSumGrades(grades);
+  return sum / grades.length;
 }
 
-export function getPassingGrades(grades) {
-   
+export function getPassingGrades(grades: number[]): number[] {
+  return grades.filter((grade) => grade >= 10);
 }
 
-
-export function getFailingGrades(grades) {
-    
+export function getFailingGrades(grades: number[]): number[] {
+  return grades.filter((grade) => grade <= 9);
 }
 
-export function getRaisedGrades(grades) {
-    
+export function getRaisedGrades(grades: number[]): number[] {
+  return grades.map((grade) => (grade < 20 ? grade + 1 : grade));
 }
